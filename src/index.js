2dc82ee7f4b1f91e804ef7bdf1fa40f7e1341a6c
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-05-12 17:19:38 +0000

    no mentions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     execute: function (message, args) {
-        message.reply(`http://image.thum.io/get/${args[0]}`);
+        message.reply({ content: `http://image.thum.io/get/${args[0]}`, allowedMentions: { parse: [] }});
     }
 }
