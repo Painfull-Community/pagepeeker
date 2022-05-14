3399407b41af746722531bcbf6abf9d1fbc9d657
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-05-14 11:59:19 +0000

    fix stupid user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 module.exports = {
     execute: function (message, args, utils) {
         var cls = utils.apis["core-cls"].api;
+        if(!args[0]) return message.reply(cls.getString("pagepeeker", "noargs"));
         if(!args[0].startsWith("http")) args[0] = "http://" + args[0];
         message.reply(cls.getString("pagepeeker", "wait"))
-        message.channel.send({ content: `http://image.thum.io/get/${args[0]}`, allowedMentions: { parse: [] }});
+        message.channel.send({ content: `https://image.thum.io/get/${args[0]}`, allowedMentions: { parse: [] }});
     }
 }
